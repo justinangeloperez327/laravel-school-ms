910447582a3bc6d5909a65ab743b7aa4b6428cd4
justin <justin@redhq.com.au>
2025-05-30 13:04:28 +0400

    first commit

---
+++ app/Http/Controllers/CLassSubjectController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CLassSubject;
+use App\Http\Requests\StoreCLassSubjectRequest;
+use App\Http\Requests\UpdateCLassSubjectRequest;
+
+class CLassSubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCLassSubjectRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CLassSubject $cLassSubject)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CLassSubject $cLassSubject)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCLassSubjectRequest $request, CLassSubject $cLassSubject)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CLassSubject $cLassSubject)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/StudentFeeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudentFee;
+use App\Http\Requests\StoreStudentFeeRequest;
+use App\Http\Requests\UpdateStudentFeeRequest;
+
+class StudentFeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStudentFeeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudentFee $studentFee)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StudentFee $studentFee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStudentFeeRequest $request, StudentFee $studentFee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StudentFee $studentFee)
+    {
+        //
+    }
+}
